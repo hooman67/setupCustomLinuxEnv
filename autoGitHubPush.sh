fc328e077931d9d28cbc100ca8c8e0e56cf13523
Hooman Shariati <houman_sh2001@hotmail.com>
2018-05-11 11:23:21 -0700

    Fixed the empty space before assigment that was causing comit to not happen

--- autoGitHubPush.sh
+++ autoGitHubPush.sh
@@ -4,15 +4,22 @@ curDir=`pwd`
 
 cd "/home/hooman/teammembers/hooman"
 
-cp /home/hooman/dataPreparation/hsDataExploration.ipynb /home/hooman/teammembers/hooman
-cp /home/hooman/dataPreparation/hsNetworkRunner.ipynb /home/hooman/teammembers/hooman
 
-git add "./hsDataExploration.ipynb"
-git add "./hsNetworkRunner.ipynb"
+#cp /home/hooman/dataPreparation/hsDataExploration.ipynb /home/hooman/teammembers/hooman
+#cp /home/hooman/dataPreparation/hsNetworkRunner.ipynb /home/hooman/teammembers/hooman
+cp /home/hooman/dataPreparation/* /home/hooman/teammembers/hooman
 
-curT = `date -u`
 
-git commit -m "$curT"
+#git add "./hsDataExploration.ipynb"
+#git add "./hsNetworkRunner.ipynb"
+git add "./*"
+
+
+curT=`date -u`
+
+echo "$curT"
+
+git commit -m "hs auto generated commit at: $curT"
 git push
 
 cd $curDir
